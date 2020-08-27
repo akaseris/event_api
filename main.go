eeb8bbc23fc63d63049fb3f981d69310a83d9516
akaseris <kasseristas@gmail.com>
2020-08-27 09:31:59 +0300

    moved parts from request handler to session handlers to make code prettier

--- main.go
+++ main.go
@@ -12,46 +12,71 @@ import (
 
 var acceptedTypes = []string{"SESSION_START", "EVENT", "SESSION_END"}
 
-func handleSessionStart(jsonData []map[string]interface{}, w http.ResponseWriter) bool {
-	if str, ok := jsonData[0]["session_id"].(string); ok {
-		found := session.Find(str)
-		if found > -1 {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(`{"error": "Session already exists"}`))
-			return false
-		} else if found < -1 {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(`{"error": "Error searching for sessions"}`))
-			return false
-		} else {
-			session.Add(str)
-			return true
+func handleSessionStart(jsonData []map[string]interface{}, w http.ResponseWriter, paramID []string, paramIDIndex int) bool {
+	// Check if the first JSON is of type SESSION_START and it matches the id on the parameters
+	if jsonData[0]["type"] == "SESSION_START" && jsonData[0]["session_id"] == paramID[0] {
+		if str, ok := jsonData[0]["session_id"].(string); ok {
+			found := session.Find(str)
+			if found > -1 {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(`{"error": "Session already exists"}`))
+				return false
+			} else if found < -1 {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(`{"error": "Error searching for sessions"}`))
+				return false
+			} else {
+				session.Add(str)
+				return true
+			}
 		}
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error": "Start session is not a string"}`))
+		return false
+		// Else catch he case where it does not match the one on the parameters
+	} else if jsonData[0]["type"] == "SESSION_START" && jsonData[0]["session_id"] != paramID[0] {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error": "session_id in body does not match session_id in parameters"}`))
+		return false
+		// Else catch the case where the first JSON was not of type SESSION_START and the id on the parameters is not in the active sessions
+	} else if paramIDIndex < 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error": "session_id in parameters does not exist and there is no SESSION_START EVENT"}`))
+		return false
 	}
-	w.WriteHeader(http.StatusBadRequest)
-	w.Write([]byte(`{"error": "Start session is not a string"}`))
-	return false
+	// Return true if no condition is met to continue with the request
+	return true
 }
 
-func handleSessionEnd(jsonData []map[string]interface{}, w http.ResponseWriter) bool {
-	if str, ok := jsonData[len(jsonData)-1]["session_id"].(string); ok {
-		found := session.Find(str)
-		if found == -1 {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte(`{"error": "End session does not exist"}`))
-			return false
-		} else if found < -1 {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(`{"error": "Error searching for sessions"}`))
-			return false
-		} else {
-			session.Remove(found)
-			return true
+func handleSessionEnd(jsonData []map[string]interface{}, w http.ResponseWriter, paramID []string) bool {
+	// Check if the first JSON is of type SESSION_END and it matches the id on the parameters
+	if jsonData[len(jsonData)-1]["type"] == "SESSION_END" && jsonData[len(jsonData)-1]["session_id"] == paramID[0] {
+		if str, ok := jsonData[len(jsonData)-1]["session_id"].(string); ok {
+			found := session.Find(str)
+			if found == -1 {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(`{"error": "End session does not exist"}`))
+				return false
+			} else if found < -1 {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(`{"error": "Error searching for sessions"}`))
+				return false
+			} else {
+				session.Remove(found)
+				return true
+			}
 		}
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error": "Session is not a string"}`))
+		return false
+		// Else catch he case where it does not match the one on the parameters
+	} else if jsonData[len(jsonData)-1]["type"] == "SESSION_END" && jsonData[len(jsonData)-1]["session_id"] != paramID[0] {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"error": "session_id in body does not match session_id in parameters"}`))
+		return false
 	}
-	w.WriteHeader(http.StatusBadRequest)
-	w.Write([]byte(`{"error": "Session is not a string"}`))
-	return false
+	// Return true if no condition is met to continue with the request
+	return true
 }
 
 func get(w http.ResponseWriter, r *http.Request) {
@@ -98,30 +123,14 @@ func post(w http.ResponseWriter, r *http.Request) {
 	paramIDIndex := session.Find(string(paramID[0]))
 
 	// Handle SESSION_START
-	if jsonData[0]["type"] == "SESSION_START" && jsonData[0]["session_id"] == paramID[0] {
-		if ok := handleSessionStart(jsonData, w); !ok {
-			return
-		}
-	} else if jsonData[0]["type"] == "SESSION_START" && jsonData[0]["session_id"] != paramID[0] {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"error": "session_id in body does not match session_id in parameters"}`))
-		return
-	} else if paramIDIndex < 0 {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"error": "session_id in parameters does not exist and there is no SESSION_START EVENT"}`))
+	if ok := handleSessionStart(jsonData, w, paramID, paramIDIndex); !ok {
 		return
 	}
 
 	// Handle EVENT
 
 	// Handle SESSION_END
-	if jsonData[len(jsonData)-1]["type"] == "SESSION_END" && jsonData[len(jsonData)-1]["session_id"] == paramID[0] {
-		if ok := handleSessionEnd(jsonData, w); !ok {
-			return
-		}
-	} else if jsonData[len(jsonData)-1]["type"] == "SESSION_END" && jsonData[len(jsonData)-1]["session_id"] != paramID[0] {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"error": "session_id in body does not match session_id in parameters"}`))
+	if ok := handleSessionEnd(jsonData, w, paramID); !ok {
 		return
 	}
 
